d5fe07b19ef011e07e34907f12ef7a665473caeb
Levi Meslan <534143@student.glu.nl>
2021-02-02 01:15:19 +0100

    Volledige Portfolio Ish

---
+++ resources/views/base/base.blade.php
@@ -0,0 +1,119 @@
+<?php
+use TCG\Voyager\Models\Post;
+$posts = TCG\Voyager\Models\Post::all();
+?>
+
+
+<body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+            <div class="container">
+                <a class="navbar-brand js-scroll-trigger" href="#page-top">Levi Meslan</a>
+                <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    Menu
+                    <i class="fas fa-bars"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#about">Home</a></li>
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#projects">Projects</a></li>
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#contact">Contact</a></li>
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="admin">Admin</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <!-- About-->
+        <section class="about-section text-center" id="about">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 mx-auto">
+                        <h2 class="text-white mb-4">Home</h2>
+                        <p class="text-white-50">
+                            Dit is de home page van mijn portfolio
+                        </p>
+                    </div>
+                </div>
+        </section>
+
+        <!-- Projects-->
+        <section class="projects-section bg-light" id="projects">
+            <div class="container">
+                <!-- Featured Project Row-->
+                <div class="row">
+                    @if (count($posts) > 0)
+                        @foreach ($posts as $post)
+                                <div class="col-md-4">
+                                    <figure class="card card-product">
+                                        <div class="img-wrap"><img src="{{$post->image}}"></div>
+                                        <div class="info-wrap">
+                                            <div class="rating-wrap">
+                                                <div class="label-rating"><p style="color: #000">{{$post->title}}</p></div>
+                                            </div> <!-- rating-wrap.// -->
+                                        </div>
+                                        <div class="bottom-wrap">
+                                            <a href="{{$post->excerpt}}" class="btn btn-md" style="background-color: #18d26e; margin-left: 20%">Go to project</a><!-- price-wrap.// -->
+                                        </div> <!-- bottom-wrap.// -->
+                                    </figure>
+                                </div>
+                                @endforeach
+                    @endif
+                </div>
+
+            
+
+        <!-- Contact-->
+        <section class="contact-section bg-black" id="contact">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4 mb-3 mb-md-0">
+                        <div class="card py-4 h-100">
+                            <div class="card-body text-center">
+                                <i class="fas fa-map-marked-alt text-primary mb-2"></i>
+                                <h4 class="text-uppercase m-0">Location</h4>
+                                <hr class="my-4" />
+                                <div class="small text-black-50">Geertruidenberg, Noord-Brabant</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 mb-3 mb-md-0">
+                        <div class="card py-4 h-100">
+                            <div class="card-body text-center">
+                                <i class="fas fa-envelope text-primary mb-2"></i>
+                                <h4 class="text-uppercase m-0">Email</h4>
+                                <hr class="my-4" />
+                                <div class="small text-black-50"><a href="#!">levimeslan@gmail.com</a></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 mb-3 mb-md-0">
+                        <div class="card py-4 h-100">
+                            <div class="card-body text-center">
+                                <i class="fas fa-mobile-alt text-primary mb-2"></i>
+                                <h4 class="text-uppercase m-0">Phone</h4>
+                                <hr class="my-4" />
+                                <div class="small text-black-50">+1 (555) 902-8832</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="social d-flex justify-content-center">
+                    <a class="mx-2" href="#!"><i class="fab fa-twitter"></i></a>
+                    <a class="mx-2" href="#!"><i class="fab fa-facebook-f"></i></a>
+                    <a class="mx-2" href="https://github.com/Iamcrazylevi/Portfolio"><i class="fab fa-github"></i></a>
+                </div>
+            </div>
+        </section>
+
+        <!-- Footer-->
+        <footer class="footer bg-black small text-center text-white-50"><div class="container">Copyright © Your Website 2020</div></footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Third party plugin JS-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel Project</title>
+</head>
+<body>
+    <h1>Contact</h1>
+
+            <div class="links">
+                <a href="http://portfolio.test">Home</a>
+                <a href="http://portfolio.test/about">About</a>
+                <a href="http://portfolio.test/work">Work</a>
+            </div>
+</body>
+</html>
