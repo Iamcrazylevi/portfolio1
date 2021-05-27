cd0b6c5bfff5dec9880cf8a76db7db4a067ccbfc
Levi Meslan <534143@student.glu.nl>
2021-05-27 09:23:24 +0200

    Portfolio Mostly Done
    Mostly done version

--- resources/views/base/base.blade.php
+++ resources/views/base/base.blade.php
@@ -29,9 +29,13 @@ $posts = TCG\Voyager\Models\Post::all();
             <div class="container">
                 <div class="row">
                     <div class="col-lg-8 mx-auto">
-                        <h2 class="text-white mb-4">Home</h2>
+                        <h2 class="text-white mb-4">About Me</h2>
                         <p class="text-white-50">
-                            Hi! Welcome to my portfolio!
+                            Hi! My name is Levi Meslan and I'm 18 years old. I am still ways away from being an experienced programmer. But I still want to learn more about programming. Next to programming I like to game and listen to music.
+                            <br><br>
+                            My hard skills are:<br>PHP<br>HTML<br>CSS<br>Javascript.
+                            <br><br>
+                            My soft skills are: <br>Team Work<br>Adaptability
                         </p>
                     </div>
                 </div>
@@ -49,11 +53,11 @@ $posts = TCG\Voyager\Models\Post::all();
                                         <div class="img-wrap"><img src="{{$post->image}}"></div>
                                         <div class="info-wrap">
                                             <div class="rating-wrap">
-                                                <div class="label-rating"><p style="color: #000">{{$post->title}}</p></div>
+                                                <div class="label-rating"><p style="color: #00000;">{{$post->title}}</p></div>
                                             </div> <!-- rating-wrap.// -->
                                         </div>
                                         <div class="bottom-wrap">
-                                            <a href="{{$post->excerpt}}" class="btn btn-md" style="background-color: #18d26e; margin-left: 20%">Go to project</a><!-- price-wrap.// -->
+                                            <a href="{{$post->excerpt}}" class="btn btn-md" style="background-color: rgba(152, 4, 221, 0.9); margin-left: 20%">Go to project</a><!-- price-wrap.// -->
                                         </div> <!-- bottom-wrap.// -->
                                     </figure>
                                 </div>
@@ -64,7 +68,7 @@ $posts = TCG\Voyager\Models\Post::all();
             
 
         <!-- Contact-->
-        <section class="contact-section bg-black" id="contact">
+        <section class="contact-section" id="contact">
             <div class="container">
                 <div class="row">
                     <div class="col-md-4 mb-3 mb-md-0">
@@ -73,7 +77,7 @@ $posts = TCG\Voyager\Models\Post::all();
                                 <i class="fas fa-map-marked-alt text-primary mb-2"></i>
                                 <h4 class="text-uppercase m-0">Location</h4>
                                 <hr class="my-4" />
-                                <div class="small text-black-50">Geertruidenberg, Noord-Brabant</div>
+                                <div class="small text-black-50"><a href="#!">Utrecht, The Netherlands</a></div>
                             </div>
                         </div>
                     </div>
@@ -83,7 +87,9 @@ $posts = TCG\Voyager\Models\Post::all();
                                 <i class="fas fa-envelope text-primary mb-2"></i>
                                 <h4 class="text-uppercase m-0">Email</h4>
                                 <hr class="my-4" />
-                                <div class="small text-black-50"><a href="#!">levimeslan@gmail.com</a></div>
+                                <script language="javascript" type="text/javascript"> var pre = "levimeslan";
+                                    document.write("<p><a href='mailto:" + pre + "@gmail.com'>" + pre + "@gmail.com</a></p>"); </script>
+                                <noscript>Enable javascript to see my email!</noscript>
                             </div>
                         </div>
                     </div>
@@ -91,23 +97,17 @@ $posts = TCG\Voyager\Models\Post::all();
                         <div class="card py-4 h-100">
                             <div class="card-body text-center">
                                 <i class="fas fa-mobile-alt text-primary mb-2"></i>
-                                <h4 class="text-uppercase m-0">Phone</h4>
+                                <h4 class="text-uppercase m-0">Linkedin</h4>
                                 <hr class="my-4" />
-                                <div class="small text-black-50">+1 (555) 902-8832</div>
+                                <div class="small text-black-50"><a href="https://www.linkedin.com/in/levi-meslan-3917b9212/">Linkedin</a></div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="social d-flex justify-content-center">
-                    <a class="mx-2" href="#!"><i class="fab fa-twitter"></i></a>
-                    <a class="mx-2" href="#!"><i class="fab fa-facebook-f"></i></a>
-                    <a class="mx-2" href="https://github.com/Iamcrazylevi"><i class="fab fa-github"></i></a>
-                </div>
             </div>
+            <p class= "copyright" style="text-align: center">Copyright &copy; <script>document.write(new Date().getFullYear())</script> Levi Meslan All Rights Reserved</p>
         </section>
 
-        <!-- Footer-->
-        <footer class="footer bg-black small text-center text-white-50"><div class="container">Copyright © Your Website 2020</div></footer>
         <!-- Bootstrap core JS-->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,9 +2,7 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                        <a href="{{ url('/admin') }}">Admin</a>
-                    @else
-                        <a href="/admin">Login</a> |
+                    
                     @endauth
                 </div>
             @endif
